532824e6618644dd3e161cffd53bca8f5721bc5b
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-01-12 17:49:52 -0800

    made package.swift dynamic

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "NnSwiftUIKit",
+            type: .dynamic,
             targets: ["NnSwiftUIKit"]),
     ],
     targets: [
