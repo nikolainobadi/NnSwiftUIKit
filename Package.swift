a1b8958c62899fdc74a6c3587cc437bbad4ce772
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-01-07 15:57:42 -0800

    removed dynamic parameter from package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,6 @@ let package = Package(
     products: [
         .library(
             name: "NnSwiftUIKit",
-            type: .dynamic,
             targets: ["NnSwiftUIKit"]),
     ],
     targets: [
