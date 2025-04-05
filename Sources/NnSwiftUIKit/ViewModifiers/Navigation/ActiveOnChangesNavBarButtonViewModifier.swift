f5ef25c2bbf825fe384984bb688161d647c12ac4
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-04-04 21:12:20 -0700

    added new ActiveOnChangeNavBarButtonViewModifier

---
+++ Sources/NnSwiftUIKit/ViewModifiers/Navigation/ActiveOnChangesNavBarButtonViewModifier.swift
@@ -0,0 +1,34 @@
+//
+//  ActiveOnChangesNavBarButtonViewModifier.swift
+//  NnSwiftUIKit
+//
+//  Created by Nikolai Nobadi on 4/4/25.
+//
+
+import SwiftUI
+
+@available(iOS 17.0, *)
+struct ActiveOnChangesNavBarButtonViewModifier<Item: Equatable>: ViewModifier {
+    @State private var itemDidChange = false
+    
+    let item: Item
+    let font: Font
+    let textColor: Color
+    let accessibilityId: String?
+    let placement: ToolbarItemPlacement
+    let buttonContent: NavBarButtonContent
+    let action: () async throws -> Void
+    
+    func body(content: Content) -> some View {
+        content
+            .trackingItemChanges(item: item, itemDidChange: $itemDidChange)
+            .withNavBarButton(placement: placement, buttonContent: buttonContent, font: font, textColor: textColor, isActive: itemDidChange, accessibilityId: accessibilityId, action: action)
+    }
+}
+
+@available(iOS 17.0, *)
+public extension View {
+    func activeOnChangeNavBarButton<Item: Equatable>(item: Item, font: Font = .title2, textColor: Color = .primary, accessibilityId: String? = nil, placement: ToolbarItemPlacement, buttonContent: NavBarButtonContent, action: @escaping () async throws -> Void) -> some View {
+        modifier(ActiveOnChangesNavBarButtonViewModifier(item: item, font: font, textColor: textColor, accessibilityId: accessibilityId, placement: placement, buttonContent: buttonContent, action: action))
+    }
+}
