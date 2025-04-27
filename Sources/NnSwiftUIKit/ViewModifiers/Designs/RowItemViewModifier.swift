ae3ba9141f313b8869a957e1e68a976ed987044b
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-04-27 12:55:29 -0700

    minor update to inline docs

--- Sources/NnSwiftUIKit/ViewModifiers/Designs/RowItemViewModifier.swift
+++ Sources/NnSwiftUIKit/ViewModifiers/Designs/RowItemViewModifier.swift
@@ -30,6 +30,13 @@ struct RowItemViewModifier: ViewModifier {
 }
 
 public extension View {
+    /// Applies a horizontal layout to the view, optionally adding a chevron and setting alignment, tint, and max width.
+    ///
+    /// - Parameters:
+    ///   - withChevron: A Boolean value indicating whether to display a chevron on the trailing side. Defaults to `false`.
+    ///   - maxWidth: The maximum width the view can occupy. Defaults to `.infinity`.
+    ///   - alignment: The horizontal alignment for the content. Defaults to `.leading`.
+    ///   - tint: The color used for the chevron, if displayed. Defaults to `.primary`.
     func asRowItem(withChevron: Bool = false, maxWidth: CGFloat = .infinity, alignment: Alignment = .leading, tint: Color = .primary) -> some View {
         modifier(RowItemViewModifier(withChevron: withChevron, tint: tint, maxWidth: maxWidth, alignment: alignment))
     }
--- Sources/NnSwiftUIKit/ViewModifiers/Utility/TappableRowViewModifier.swift
+++ Sources/NnSwiftUIKit/ViewModifiers/Utility/TappableRowViewModifier.swift
@@ -14,33 +14,50 @@ struct TappableRowViewModifier: ViewModifier {
     let tint: Color
     let tapIsActive: Bool
     let withChevron: Bool
+    let maxWidth: CGFloat
     let alignment: Alignment
     let hideLoadingIndicator: Bool
     let action: () async throws -> Void
     
     func body(content: Content) -> some View {
         content
-            .asRowItem(withChevron: withChevron, alignment: alignment, tint: tint)
-            .showingConditionalView(when: tapIsActive) {
-                content
-                    .asRowItem(withChevron: withChevron, alignment: alignment, tint: tint)
-                    .onTapGesture {
-                        context.performAction(hideLoadingIndicator: hideLoadingIndicator, action: action)
-                    }
-            }
+            .asRowItem(withChevron: withChevron, maxWidth: maxWidth, alignment: alignment, tint: tint)
+            .modifier(TapModifier(isActive: tapIsActive, hideLoadingIndicator: hideLoadingIndicator, action: action))
     }
 }
 
 public extension View {
     /// Configures the view as a tappable row item with customizable appearance and behavior.
+    ///
     /// - Parameters:
     ///   - tapIsActive: A Boolean indicating whether the tap action is active.
     ///   - withChevron: A Boolean indicating if a chevron should be displayed on the right.
+    ///   - maxWidth: The maximum width the view can occupy. Defaults to `.infinity`.
     ///   - tint: The tint color for the chevron, defaulting to primary.
     ///   - alignment: The alignment of the content within the row item, defaulting to leading.
+    ///   - hideLoadingIndicator: A Boolean indicating whther or not async methods should include a loading indicator. Defaults to `false`.
     ///   - onTapGesture: The action to perform when the row is tapped.
-    /// - Returns: A modified view configured as a tappable row item.
-    func tappable(tapIsActive: Bool = true, withChevron: Bool = false, tint: Color = .primary, alignment: Alignment = .leading, hideLoadingIndicator: Bool = false, onTapGesture: @escaping () async throws -> Void) -> some View {
-        modifier(TappableRowViewModifier(tint: tint, tapIsActive: tapIsActive, withChevron: withChevron, alignment: alignment, hideLoadingIndicator: hideLoadingIndicator, action: onTapGesture))
+    func tappable(tapIsActive: Bool = true, withChevron: Bool = false, maxWidth: CGFloat = .infinity, tint: Color = .primary, alignment: Alignment = .leading, hideLoadingIndicator: Bool = false, onTapGesture: @escaping () async throws -> Void) -> some View {
+        modifier(TappableRowViewModifier(tint: tint, tapIsActive: tapIsActive, withChevron: withChevron, maxWidth: maxWidth, alignment: alignment, hideLoadingIndicator: hideLoadingIndicator, action: onTapGesture))
+    }
+}
+
+
+// MARK: - Helpers
+private struct TapModifier: ViewModifier {
+    @EnvironmentObject var context: NnErrorHandlingContext
+    
+    let isActive: Bool
+    let hideLoadingIndicator: Bool
+    let action: () async throws -> Void
+
+    func body(content: Content) -> some View {
+        content
+            .showingConditionalView(when: isActive) {
+                content
+                    .onTapGesture {
+                        context.performAction(hideLoadingIndicator: hideLoadingIndicator, action: action)
+                    }
+            }
     }
 }
